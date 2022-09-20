eb47dfae2d64dfdf42a30a4296af118c160e0aa6
Fernando Milagres Menezes <milagres.fernando@gmail.com>
2022-09-20 10:05:40 -0300

    ajuste erro saiba mais 2

---
+++ src/screens/page4/customComponents/operacoesDocumentos/blocoSaibaMais/index.js
@@ -0,0 +1,40 @@
+// React Elements/Hooks
+import { Fragment } from "react";
+
+// Components
+import { Container, Row, Col } from "react-bootstrap";
+
+import Destaques from "../../../../../components/destaques";
+
+function BlocoSaibaMais() {
+  const textsBlock = [
+    {
+      tagElement: "p",
+      className: "",
+      content: (
+        <Fragment>
+          <a href="https://www.cdhu.sp.gov.br/" target="_blank">
+            <strong>Clique aqui</strong>
+          </a>{" "}
+          para acessar os conteúdos relacionados à LGPD. Lembre-se, também, de
+          utilizar o e-mail <u>lgpd@cdhu.sp.gov.br</u> para obter informações e
+          esclarecimentos relacionados à aplicação da LGPD em seu dia a dia de
+          trabalho.
+        </Fragment>
+      ),
+    },
+  ];
+  return (
+    <Row className="justify-content-center">
+      <Col lg="10">
+        <Destaques
+          title="Saiba Mais"
+          className="saibaMais mt-4 mb-5"
+          textsBlock={textsBlock}
+        />
+      </Col>
+    </Row>
+  );
+}
+
+export default BlocoSaibaMais;
